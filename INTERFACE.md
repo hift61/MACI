484b96d811638c3ea624fc64a6b35c35803e5f89
HENRY KIM <kimh8955@gmail.com>
2026-08-25 15:19:08 +0900

    Edit world_core.py
    클래스 'GameMap' 정의
    맵의 width와 height를 입력받는 함수 정의
    'INTERFACE.md'에 'GameMAP'에 대한 함수들 정의

--- INTERFACE.md
+++ INTERFACE.md
@@ -5,7 +5,14 @@ This document defines the functions shared between modules, including their para
 --- 
 ## world_core.py
 
-No elements yet.
+- GameMap(Class)
+
+self.map_width : 테스트 환경의 좌우 길이
+self.map_height : 테스트 환경의 상하 길이
+
+### GameMap
+
+
 
 ---
 ## enviroment.py
--- world_core.py
+++ world_core.py
@@ -1,5 +1,39 @@
-import pygame
+# Main library
 import sys
+import pygame
+
+# Console library
+from rich.console import Console
+from rich.panel import Panel
+from rich.prompt import IntPrompt
+
+console = Console()
+
+class GameMap:
+    def __init__(self):
+        self.map_width = 500
+        self.map_height = 500
+
+    def configure_map(self):
+        # Input map data
+        self.map_width = IntPrompt.ask(
+            "[bold magenta]Width[/]",
+            default=500
+        )
+
+        self.map_height = IntPrompt.ask(
+            "[bold magenta]Height[/]",
+            default=500
+        )
+
+        console.print(
+            f"[green]Map size:[/] {self.map_width} × {self.map_height}"
+        )
+
 
 if __name__ == "__main__":
-  print("hello world!")
+    console.print("[bold cyan italic]Project MACI[/]")
+    console.rule()
+
+    game_map = GameMap()
+    game_map.configure_map()
