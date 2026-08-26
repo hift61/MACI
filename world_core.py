d6e83f2a4ff4c6fa9795da312f106530fc949d32
HENRY KIM <kimh8955@gmail.com>
2026-08-26 19:07:22 +0900

    Update world_core.py
    내일 수정할 것:
    _generate_map
    함수 파라미터 오류

--- world_core.py
+++ world_core.py
@@ -25,6 +25,38 @@ class GameMap:
         self.window_width = self.map_width + 50
         self.window_height = self.map_height + 50
     
+    def _generate_walls(self, wall_c: int, seed: int) -> list[Wall]:
+        rng = random.Random(seed)
+        walls: list[Wall] = []
+        
+        i = 0
+        while len(walls) < wall_c and i < wall_c * 20:
+            i += 1
+            
+            tmp = self._make_random_wall(rng)
+            
+            if any(tmp.rect.colliderect(wall.rect) for wall in walls):
+                continue
+            
+            walls.append(tmp)
+        
+        return walls
+    
+    def _make_random_wall(self, rng: random.Random) -> Wall:
+        orientation = rng.choice(["h", "v"])
+        
+        if orientation == "h":
+            width = rng.randint(80, 200)
+            height = rng.randint(10, 25)
+        else:
+            width = rng.randint(10, 25)
+            height = rng.randint(80, 200)
+        
+        x = rng.randint(25, 25 + self.map_width - width)
+        y = rng.randint(25, 25 + self.map_height - height)
+        
+        return Wall(x, y, width, height)
+    
     def display_map(self) -> None:
         pygame.init()
         
@@ -52,6 +84,10 @@ class GameMap:
             
             for b in border:
                 pygame.draw.rect(screen, "white", b.rect)
+            
+            for wall in self._generate_walls():
+                pygame.draw.rect(screen, "white", wall.rect)
+            
             pygame.display.flip()
         
         pygame.quit()
@@ -78,7 +114,7 @@ class GameMap:
     
     # Input map data
     def configure_map(self) -> None:
-        self.map_width = IntPrompt.ask( "[bold magenta]Width[/]", default=500)
+        self.map_width = IntPrompt.ask("[bold magenta]Width[/]", default=500)
         self.window_width = self.map_width + 50
 
         self.map_height = IntPrompt.ask("[bold magenta]Height[/]", default=500)
@@ -94,19 +130,17 @@ if __name__ == "__main__":
                 "[bold bright_cyan]M A C I[/]"
             ),
             border_style="bright_magenta",
-            box = box.DOUBLE,
+            box=box.DOUBLE,
             padding=(1, 6),
             expand=False
-            )
         )
+    )
     console.rule()
-
+    
+    # 맵 정의
     game_map = GameMap()
     game_map.configure_map()
     
     print(game_map.window_width, game_map.window_height)
     
-    print(game_map.make_rseed())
-    print(game_map.make_seed(12, 100611))
-    print(game_map.read_seed('400x400-12-987654'))
     game_map.display_map()
